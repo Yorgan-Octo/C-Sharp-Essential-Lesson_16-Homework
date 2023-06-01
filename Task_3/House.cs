ee9cbd4329c7a3de2ea8eedd969f5de6da5583cd
Artem Priadchenko <yorgan.info@gmail.com>
2023-06-01 22:20:53 +0300

    Завершение задания

---
+++ Task_3/House.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+
+    class A 
+    {
+        public int a = 5;
+    }
+
+    class B
+    {
+        public int b = 3;
+    }
+
+
+    internal class House
+    {
+
+        private A room1 = new A();
+        private B room2 = new B();
+
+
+        public int Room1 { get { return room1.a; } set { room1.a = value; } }
+        public int Room2 { get { return room2.b; } set { room2.b = value; } }
+
+        public House(int room1, int room2)
+        {
+            Room1 = room1;
+            Room2 = room2;
+        }
+
+        public House DeepClone()
+        {
+            return new House(this.Room1, this.Room2);
+        }
+
+
+        public House Clone()
+        {
+            House clone = this.MemberwiseClone() as House;
+
+            return clone;
+        }
+
+
+    }
+}
---
+++ Task_3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+
+            House house = new House(4,5);
+
+
+            House houseClone = house.DeepClone() as House;
+
+            House houseCloneNotDeep = house.Clone();
+
+            Console.WriteLine(house.Room1);
+            Console.WriteLine(house.Room2);
+            Console.WriteLine("--------------------");
+            Console.WriteLine(houseClone.Room1);
+            Console.WriteLine(houseClone.Room2);
+            Console.WriteLine("--------------------");
+            Console.WriteLine(houseCloneNotDeep.Room1);
+            Console.WriteLine(houseCloneNotDeep.Room2);
+            Console.WriteLine("--------------------");
+
+            house.Room1 = 12;
+            house.Room2 = 16;
+
+
+            Console.WriteLine(house.Room1);
+            Console.WriteLine(house.Room2);
+            Console.WriteLine("--------------------");
+            Console.WriteLine(houseClone.Room1);
+            Console.WriteLine(houseClone.Room2);
+            Console.WriteLine("--------------------");
+            Console.WriteLine(houseCloneNotDeep.Room1);
+            Console.WriteLine(houseCloneNotDeep.Room2);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
