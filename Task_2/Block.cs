d4cd7c7e5f484a46643c6d65066898350a91672c
Artem Pr <yorgan.info@gmail.com>
2023-06-01 16:38:21 +0300

    Добавьте файлы проекта.

---
+++ Task_2/Block.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    internal class Block
+    {
+
+        protected int right, left, up, down;
+
+
+        public Block(int right, int left, int up, int down)
+        {
+            this.right = right;
+            this.left = left;
+            this.up = up;
+            this.down = down;
+        }
+
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || this.GetType() != obj.GetType())
+                return false;
+
+            Block b = (Block)obj;
+
+            return right == b.right && left == b.left && up == b.up && down == b.down;
+
+        }
+
+
+        public override string ToString()
+        {
+            return $"Права сторона = {right} см\nЛіва сторона = {left} см\nВерзня сторона = {up} см\nНижня сторона = {down} см";
+        }
+
+
+    }
+}
---
+++ Task_2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            Block block1 = new Block(2, 2, 4, 4);
+            Block block2 = new Block(2, 2, 2, 2);
+            Block block3 = new Block(6, 7, 8, 3);
+            Block block4 = new Block(6, 7, 8, 3);
+
+            Console.WriteLine(new String('=', 50));
+
+            Console.WriteLine("Порівняння:");
+            Console.WriteLine(new String('=', 50));
+
+            Console.WriteLine($"block1 == block2: {block1.Equals(block2)}");
+            Console.WriteLine($"block2 == block3: {block2.Equals(block3)}");
+            Console.WriteLine($"block3 == block4: {block3.Equals(block4)}");
+            Console.WriteLine($"block4 == block1: {block4.Equals(block1)}");
+
+            Console.WriteLine();
+            Console.WriteLine(new String('=',50));
+
+            Console.WriteLine("Інформація про блоки:");
+            Console.WriteLine(new String('=', 50));
+            Console.WriteLine("block1:");
+            Console.WriteLine(block1.ToString());
+            Console.WriteLine(new String('-', 50));
+            Console.WriteLine("block2:");
+            Console.WriteLine(block2.ToString());
+            Console.WriteLine(new String('-', 50));
+            Console.WriteLine("block3:");
+            Console.WriteLine(block3.ToString());
+            Console.WriteLine(new String('-', 50));
+            Console.WriteLine("block4:");
+            Console.WriteLine(block4.ToString());
+
+
+            Console.ReadKey();
+        }
+    }
+}
