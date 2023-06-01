ab0086eebb8dfc30a3123fd2abd70e6da8d44a27
Artem Priadchenko <yorgan.info@gmail.com>
2023-06-02 00:34:38 +0300

    Завершение щавдання 6

---
+++ Task_6/Point.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_6
+{
+    public struct Point
+    {
+
+        public int X {  get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+
+        public Point(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public static Point operator +(Point a, Point b)
+        {
+            return new Point(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+        }
+
+        public override string ToString()
+        {
+            return $"X - {X}; Y - {Y}; Z - {Z};";
+        }
+
+    }
+}
---
+++ Task_6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_6
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            Point point1 = new Point(1, 1, 1);
+            Point point2 = new Point(2, 2, 2);
+
+
+            Point point3 = point1 + point2;
+
+
+            Console.WriteLine(point3.ToString());
+            
+            Console.ReadKey();
+
+        }
+    }
+}
