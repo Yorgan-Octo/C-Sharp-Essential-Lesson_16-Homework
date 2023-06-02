281989d363dd3c73a53b7c20971c8f7717869a31
Artem Pr <yorgan.info@gmail.com>
2023-06-02 15:10:08 +0300

    Завершення Дз

---
+++ Task_4/DateRefuge.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    internal class DateRefuge
+    {
+
+        public DateTime MyDate { get; set; }
+
+
+        public DateRefuge(string date)
+        {
+            this.MyDate = DateTime.Parse(date);
+        }
+
+        public DateRefuge()
+        {
+
+        }
+
+
+        public static DateRefuge operator +(DateRefuge a, int namdata)
+        {
+            a.MyDate = a.MyDate.AddDays(namdata);
+
+            return new DateRefuge($"{a.MyDate.Day}/{a.MyDate.Month}/{a.MyDate.Year}");
+
+        }
+
+        public static int operator -(DateRefuge a, DateRefuge b)
+        {
+            TimeSpan time = a.MyDate - b.MyDate;
+            return time.Days;
+        }
+
+
+
+    }
+}
---
+++ Task_4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            DateRefuge correntData = new DateRefuge("11/12/2022");
+
+            DateRefuge newData = new DateRefuge("11/12/2022");
+
+            Console.WriteLine(correntData.MyDate);
+
+            correntData = correntData + 12;
+
+            Console.WriteLine(correntData.MyDate);
+
+
+            int relase = correntData - newData;
+
+
+            Console.WriteLine($"Кількість днів між датами: {relase}");
+
+            Console.ReadKey();
+        }
+    }
+}
